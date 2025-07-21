b1a5351890f8e346ac028cad6436685877bfd293
Mustafa Kara <mkara@soloscripted.com>
2025-07-21 10:38:55 +0300

    feat(kms): add KmsStack for SOPS key management (#8)
    * feat(kms): add KmsStack for SOPS key management
    
    This commit introduces a new `KmsStack` to provision and manage a
    customer-managed KMS key. The key is specifically intended for use with
    SOPS (Secrets OPerationS) to encrypt and decrypt secrets.
    
    The new stack utilizes the `KmsKeyResource` construct to create a KMS key
    with the following configuration:
    - An alias of 'SopsKey' for easy identification.
    - Key rotation enabled to enhance security.
    - A removal policy of RETAIN to prevent accidental deletion of the key
      and loss of encrypted data.
    
    The stack exports the key's ARN and ID as CloudFormation outputs,
    making it easy to reference in other applications, CI/CD pipelines, or
    Terraform configurations.
    
    Signed-off-by: Mustafa Kara <mkara@soloscripted.com>
    
    * fix: lint and format
    
    Signed-off-by: Mustafa Kara <mkara@soloscripted.com>
    
    * fix: lint and format
    
    Signed-off-by: Mustafa Kara <mkara@soloscripted.com>
    
    ---------
    
    Signed-off-by: Mustafa Kara <mkara@soloscripted.com>

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 import * as cdk from 'aws-cdk-lib';
 import { IamStack } from './stacks/iam-stack';
+import { KmsStack } from './stacks/kms-stack';
 import { StorageStack } from './stacks/storage-stack';
 
 const app = new cdk.App();
@@ -8,6 +9,7 @@ const app = new cdk.App();
 const env = { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION };
 
 new IamStack(app, 'IamStack', { env });
+new KmsStack(app, 'KmsStack', { env });
 new StorageStack(app, 'StorageStack', { env });
 
 app.synth();
