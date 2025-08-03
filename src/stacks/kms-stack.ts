b24936e99242efdd4275d316cceff3df5a9fe108
Mustafa Kara <mkara@soloscripted.com>
2025-08-03 10:32:18 +0300

    introduce sops key (#12)
    Signed-off-by: Mustafa Kara <mkara@soloscripted.com>

--- src/stacks/kms-stack.ts
+++ src/stacks/kms-stack.ts
@@ -1,8 +1,26 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import { KmsKeyResource } from './security/kms-key-resource';
 
 export class KmsStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    const terraformSopsKey = new KmsKeyResource(this, 'SopsKey', {
+      alias: 'SopsKey',
+      description: 'KMS key for SOPS encryption',
+      enableKeyRotation: true,
+      removalPolicy: cdk.RemovalPolicy.RETAIN,
+    });
+
+    new cdk.CfnOutput(this, 'SopsKeyArn', {
+      value: terraformSopsKey.key.keyArn,
+      description: 'ARN of the KMS key for Terraform SOPS encryption',
+    });
+
+    new cdk.CfnOutput(this, 'SopsKeyId', {
+      value: terraformSopsKey.key.keyId,
+      description: 'ID of the KMS key for Terraform SOPS encryption',
+    });
   }
 }
