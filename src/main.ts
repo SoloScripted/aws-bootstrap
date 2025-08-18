17392af2efb4c78845d4143315294f91160ed8e8
Mustafa Kara <mkara@soloscripted.com>
2025-08-18 21:18:36 +0300

    chore:reduce costs by deeleting extra resources
    Signed-off-by: Mustafa Kara <mkara@soloscripted.com>

--- src/main.ts
+++ src/main.ts
@@ -11,15 +11,20 @@ const app = new cdk.App();
 const env = { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION };
 
 const security = new SecurityStack(app, 'SecurityStack', { env });
-const network = new NetworkStack(app, 'NetworkStack', { env });
+const network = new NetworkStack(app, 'NetworkStack', { env }, false);
 new StorageStack(app, 'StorageStack', { env });
-new AtlantisStack(app, 'AtlantisStack', {
-  env,
-  vpc: network.vpc,
-  userPool: security.googleUserPool,
-  userPoolClientIdentityProvider: cognito.UserPoolClientIdentityProvider.custom(
-    security.googleUserPoolIdentityProvider.providerName,
-  ),
-});
+new AtlantisStack(
+  app,
+  'AtlantisStack',
+  {
+    env,
+    vpc: network.vpc,
+    userPool: security.googleUserPool,
+    userPoolClientIdentityProvider: cognito.UserPoolClientIdentityProvider.custom(
+      security.googleUserPoolIdentityProvider.providerName,
+    ),
+  },
+  false,
+);
 
 app.synth();
--- src/stacks/network-stack.ts
+++ src/stacks/network-stack.ts
@@ -8,70 +8,72 @@ import { Construct } from 'constructs';
 export class NetworkStack extends cdk.Stack {
   public readonly vpc: ec2.Vpc;
 
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps, create?: boolean) {
     super(scope, id, props);
 
-    this.vpc = new ec2.Vpc(this, 'SoloScriptedVPC', {
-      ipAddresses: IpAddresses.cidr('10.0.0.0/16'),
-      subnetConfiguration: [
-        {
-          cidrMask: 19,
-          name: 'public',
-          subnetType: ec2.SubnetType.PUBLIC,
-        },
-        {
-          cidrMask: 19,
-          name: 'private-with-egress',
-          subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS,
-        },
-      ],
-      natGateways: 1,
-      maxAzs: 2,
-    });
+    if (create ?? true) {
+      this.vpc = new ec2.Vpc(this, 'SoloScriptedVPC', {
+        ipAddresses: IpAddresses.cidr('10.0.0.0/16'),
+        subnetConfiguration: [
+          {
+            cidrMask: 19,
+            name: 'public',
+            subnetType: ec2.SubnetType.PUBLIC,
+          },
+          {
+            cidrMask: 19,
+            name: 'private-with-egress',
+            subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS,
+          },
+        ],
+        natGateways: 1,
+        maxAzs: 2,
+      });
 
-    this.vpc.addInterfaceEndpoint('SecretsManagerEndpoint', {
-      service: ec2.InterfaceVpcEndpointAwsService.SECRETS_MANAGER,
-      privateDnsEnabled: true,
-    });
+      this.vpc.addInterfaceEndpoint('SecretsManagerEndpoint', {
+        service: ec2.InterfaceVpcEndpointAwsService.SECRETS_MANAGER,
+        privateDnsEnabled: true,
+      });
 
-    const privateHostedZone = new route53.PrivateHostedZone(this, 'InternalHostedZone', {
-      zoneName: 'soloscripted.internal',
-      vpc: this.vpc,
-    });
+      const privateHostedZone = new route53.PrivateHostedZone(this, 'InternalHostedZone', {
+        zoneName: 'soloscripted.internal',
+        vpc: this.vpc,
+      });
 
-    Tags.of(this.vpc).add('Name', 'soloscripted-vpc');
+      Tags.of(this.vpc).add('Name', 'soloscripted-vpc');
 
-    for (const subnet of this.vpc.publicSubnets) {
-      Tags.of(subnet).add('Tier', 'public');
+      for (const subnet of this.vpc.publicSubnets) {
+        Tags.of(subnet).add('Tier', 'public');
+      }
+
+      for (const subnet of this.vpc.privateSubnets) {
+        Tags.of(subnet).add('Tier', 'private');
+      }
+
+      new cdk.CfnOutput(this, 'VpcId', {
+        value: this.vpc.vpcId,
+        description: 'The ID of the VPC',
+      });
+
+      new cdk.CfnOutput(this, 'PublicSubnetIds', {
+        value: this.vpc.publicSubnets.map((subnet) => subnet.subnetId).join(','),
+        description: 'Comma-separated list of public subnet IDs',
+      });
+
+      new cdk.CfnOutput(this, 'PrivateSubnetIds', {
+        value: this.vpc.privateSubnets.map((subnet) => subnet.subnetId).join(','),
+        description: 'Comma-separated list of private subnet IDs',
+      });
+
+      new cdk.CfnOutput(this, 'PrivateHostedZoneId', {
+        value: privateHostedZone.hostedZoneId,
+        description: 'The ID of the private hosted zone',
+      });
+
+      new cdk.CfnOutput(this, 'PrivateHostedZoneName', {
+        value: privateHostedZone.zoneName,
+        description: 'The name of the private hosted zone',
+      });
     }
-
-    for (const subnet of this.vpc.privateSubnets) {
-      Tags.of(subnet).add('Tier', 'private');
-    }
-
-    new cdk.CfnOutput(this, 'VpcId', {
-      value: this.vpc.vpcId,
-      description: 'The ID of the VPC',
-    });
-
-    new cdk.CfnOutput(this, 'PublicSubnetIds', {
-      value: this.vpc.publicSubnets.map((subnet) => subnet.subnetId).join(','),
-      description: 'Comma-separated list of public subnet IDs',
-    });
-
-    new cdk.CfnOutput(this, 'PrivateSubnetIds', {
-      value: this.vpc.privateSubnets.map((subnet) => subnet.subnetId).join(','),
-      description: 'Comma-separated list of private subnet IDs',
-    });
-
-    new cdk.CfnOutput(this, 'PrivateHostedZoneId', {
-      value: privateHostedZone.hostedZoneId,
-      description: 'The ID of the private hosted zone',
-    });
-
-    new cdk.CfnOutput(this, 'PrivateHostedZoneName', {
-      value: privateHostedZone.zoneName,
-      description: 'The name of the private hosted zone',
-    });
   }
 }
